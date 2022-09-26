2d08a13650e58c9b4ded250aa5b7dcc7df166869
Biplab Kumar Sarkar <66509972+bipsec@users.noreply.github.com>
2022-09-26 23:49:50 +0600

    Product Items added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import Layout from "../components/Layout";
+import data from "../utils/data";
+import ProductItem from "../components/ProductItem";
 
 
-const Home = ()=> {
-  return (
-      <Layout title="Home Page">Home Page</Layout>
-  )
+const Home = () => {
+    return (
+        <Layout title="Home Page">
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+                {data.products.map((product) => (
+                        <ProductItem product={product} key={product.slug}></ProductItem>
+                    ))}
+            </div>
+        </Layout>
+    )
 }
 export default Home;
