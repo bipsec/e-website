f8b7b995686d9306f50b48a514f03eba33899a9d
Biplab Kumar Sarkar <66509972+bipsec@users.noreply.github.com>
2022-09-28 04:18:13 +0600

    loading products from MongoDB

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import Link from "next/link";
 
-const ProductItem = ({product}) => {
+const ProductItem = ({product, addToCartHandler}) => {
     return (
         <div className="card">
             <Link href={`/product/${product.slug}`}>
@@ -21,7 +21,8 @@ const ProductItem = ({product}) => {
                 </Link>
                 <p className="mb-2">{product.brand}</p>
                 <p className="mb-2">&#2547;{product.price}</p>
-                <button className="primary-button" type="button">
+                <button className="primary-button" type="button"
+                onClick={()=> addToCartHandler(product)}>
                     Add to cart
                 </button>
             </div>
